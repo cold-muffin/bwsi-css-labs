f320b94170eb54d113f2550593d42c96c83552a3
Efrat Hossain <efrathossain1130@gmail.com>
2026-02-16 13:18:52 -0500

    Add introducton to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Efrat Hossain" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("My name is Efrat Hossain. I am a junior at Stuyvesant High School. I like doing calligraphy as a hobby.")
 
 if __name__ == "__main__":
     main()
